870065e898011855eea414e8097a3edfd492baa4
Roberto Lovera <rlovera1cursos@gmail.com>
2023-07-27 21:45:38 -0400

    Primer respaldo

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+    return res.json({
+        msg: "Binevenido al Api Demo",
+    });
+});
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+});
