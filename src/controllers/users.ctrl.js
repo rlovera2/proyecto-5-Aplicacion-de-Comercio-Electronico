657607bad21c9d5a81de9fed4c861eb7798ae677
Roberto Lovera <rlovera1cursos@gmail.com>
2023-07-28 16:23:55 -0400

    varios archivos

---
+++ src/controllers/users.ctrl.js
@@ -0,0 +1,39 @@
+const obtenerUsuarios = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: "Usuarios Obtenidos",
+        data: [],
+    });
+};
+
+const crearUsuario = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: "Usuario creado",
+        data: {},
+    });
+};
+
+const actualizarUsuario = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: "Usuario actualizado",
+        data: {},
+    });
+};
+
+const eliminarUsuario = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: "Usuario eliminado",
+        data: {},
+    });
+};
+
+module.exports = {
+    obtenerUsuarios,
+    crearUsuario,
+    actualizarUsuario,
+    eliminarUsuario,
+};
+
