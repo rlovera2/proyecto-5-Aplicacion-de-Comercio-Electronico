73123a8d1699e453dafd996f081cafa66a1114d9
Roberto Lovera <rlovera1cursos@gmail.com>
2023-08-27 01:53:28 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const {dbConnection} = require("./database/config");
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
 const app = express();
 
 dbConnection();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-    return res.json({
-        msg: "Binevenido al Api Demo",
-    });
+  return res.json({
+    msg: "Binevenido al Api Demo",
+  });
 });
 
 app.use("/api/usuarios", require("./routes/users.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
